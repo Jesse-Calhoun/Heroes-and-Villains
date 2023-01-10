7b5469ae62ee4c2cef4c655446fc46a2970cbda8
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-01-10 18:23:06 -0500

    Need to delete and re-create the supers app because the Supers model in the models.py file should have been name Super, causing issues with code when I tried to just change the model name so, supers app was rebuilt + Super model rebuilt in th models.py file + admin.py file code rebuilt + serializers.py and urls.py file were created and rebuilt + finally the views.py file was rebuilt.

--- supers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Supers
-
-class SupersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Supers
-        fields = ['name','alter_ego','primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
