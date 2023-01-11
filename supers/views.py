6ec385874ded0ebe79285910a28146ea9325864b
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-01-11 11:11:31 -0500

    Adjusted serializers for both supers and super_type to include id in the fields + took the ability to be null from the super_type field inside of the Supers model + Imported status into the views.py file for supers app and began the POST request in the views file.

--- supers/views.py
+++ supers/views.py
@@ -1,11 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serliazers import SuperSerializer
 from .models import Super
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def supers_list(request):
-    supers = Super.objects.all()
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
