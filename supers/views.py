3dff8e19e1483f8322e10f19307160dd997965d0
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-01-11 11:55:24 -0500

    Created the POST request in the supers app views file + Added depth to the supers app's serializers file.

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -11,3 +12,10 @@ def supers_list(request):
         supers = Super.objects.all()
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
